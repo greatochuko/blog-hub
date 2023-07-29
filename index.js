05beddc0cc045ae313527a0db51b9c87e89fc8e6
Great Ochuko <greatochuko@gmail.com>
2023-07-30 00:33:47 +0100

    now rendering blogposts in home page

--- index.js
+++ index.js
@@ -5,8 +5,45 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
+const homeStartingContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur optio cum exercitationem quas repellendus in, cumque reiciendis? Omnis vitae architecto rerum provident rem officiis voluptatum fugiat, debitis animi eius!"
+const aboutStartingContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur optio cum exercitationem quas repellendus in, cumque reiciendis? Omnis vitae architecto rerum provident rem officiis voluptatum fugiat, debitis animi eius!"
+const contactStartingContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur optio cum exercitationem quas repellendus in, cumque reiciendis? Omnis vitae architecto rerum provident rem officiis voluptatum fugiat, debitis animi eius!"
+
+let posts = [];
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-    res.send("<h1>Blog Hub</h1>");
+    res.render("index.ejs", {
+        homeStartingContent: homeStartingContent,
+        posts: posts,
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs", {
+        aboutStartingContent: aboutStartingContent,
+    });
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs", {
+        contactStartingContent: contactStartingContent
+    });
+});
+
+app.get("/compose", (req, res) => {
+    res.render("compose.ejs");
+});
+
+app.post("/compose", (req, res) => {
+    const post = {
+        title: req.body.blogTitle,
+        content: req.body.blogContent
+    }
+    posts.push(post);
+    res.redirect("/");
 });
 
 app.listen(port, () => {
