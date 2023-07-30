88677e80509dfa8e0eafc35f08bce2115accbbec
Great Ochuko <greatochuko@gmail.com>
2023-07-30 17:06:49 +0100

    complete project

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import lodash from "lodash";
 
 const app = express();
 const port = 3000;
+const _ = lodash;
 
 const homeStartingContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur optio cum exercitationem quas repellendus in, cumque reiciendis? Omnis vitae architecto rerum provident rem officiis voluptatum fugiat, debitis animi eius!"
 const aboutStartingContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur optio cum exercitationem quas repellendus in, cumque reiciendis? Omnis vitae architecto rerum provident rem officiis voluptatum fugiat, debitis animi eius!"
@@ -35,6 +37,23 @@ app.get("/contact", (req, res) => {
 
 app.get("/compose", (req, res) => {
     res.render("compose.ejs");
+});
+
+app.get("/posts/:postTitle", (req, res) => {
+    const postTitle = _.lowerCase(req.params.postTitle);
+    let postIndex;
+
+    posts.forEach(post => {
+        if (_.lowerCase(post.title) === postTitle) {
+            console.log("Match found!");
+            postIndex = posts.indexOf(post);
+        }
+    })
+    console.log(postIndex);
+    const post = posts[postIndex];
+    res.render("post.ejs", {
+        post: post,
+    })
 });
 
 app.post("/compose", (req, res) => {
